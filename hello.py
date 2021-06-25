3a0a03410bf204be0fea175523106fc18e3fc4ce
Andrei-KV <a.v.kovalev9@gmail.com>
2021-06-25 11:26:33 +0300

    modified:   etc/nginx.conf 	new file:   hello.py

---
+++ hello.py
@@ -0,0 +1,9 @@
+import re
+
+
+def application(env, start_response):
+    string = env['QUERY_STRING']
+    result = [bytes(i + '\n', 'utf-8') for i in string.split('&')]
+    start_response('200 OK', [('Content-Type','text/plain')])
+  
+    return result
