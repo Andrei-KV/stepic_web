c78df17cf01c0d087acd9afa3841fb0973440f6a
Andrei-KV <85489769+Andrei-KV@users.noreply.github.com>
2021-09-01 21:47:21 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -18,3 +18,5 @@ sudo pip3 install --upgrade gunicorn
 sudo /etc/init.d/nginx start
 sudo /etc/init.d/nginx restart
 sudo /etc/init.d/mysql start
+mysql -uroot -e "CREATE DATABASE stepic_web;"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON stepic_web.* TO 'box'@'localhost' WITH GRANT OPTION;"
